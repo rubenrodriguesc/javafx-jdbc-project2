c1b7d995e7d7441940c8d0ab4f08b0a85ffd8b2d
Ruben C. Rodrigues <rubenrodriguesc@hotmail.com>
2019-08-05 00:01:00 -0300

    Main view

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,21 +1,29 @@
 package application;
 	
+import java.io.IOException;
+
 import javafx.application.Application;
-import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
+			Parent parent = loader.load();
+			
+			Scene mainScene = new Scene(parent);
+			primaryStage.setScene(mainScene);
+			primaryStage.setTitle("Sample JavaFX application");
+			primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/gui/icons/peopleOrange-64.png")));
+			primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/gui/icons/peopleOrange-24.png")));
 			primaryStage.show();
-		} catch(Exception e) {
+		} catch(IOException e) {
 			e.printStackTrace();
 		}
 	}
