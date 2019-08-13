7a45b29166f3e89f9d21eb1cb71ade533df8e8b1
Ruben C. Rodrigues <rubenrodriguesc@hotmail.com>
2019-08-12 22:23:50 -0300

    Adding database access to SellerService

--- src/model/services/SellerService.java
+++ src/model/services/SellerService.java
@@ -1,26 +1,17 @@
 package model.services;
 
-import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
-import model.entities.Department;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Seller;
 
 public class SellerService {
+	
+	private SellerDao dao = DaoFactory.createSellerDao();
 
 	public List<Seller> findAll() {
-		List<Seller> list = new ArrayList<>();
-
-		Department dp1 = new Department(1, "Books");
-		Department dp2 = new Department(2, "Musics");
-
-		list.add(new Seller(1, "Bob Brown", "bob@gmail.com", new Date(), 2000.0, dp1));
-		list.add(new Seller(2, "Alex Grey", "alex@gmail.com", new Date(), 3500.0, dp2));
-		list.add(new Seller(3, "Maria Green", "maria@gmail.com", new Date(), 2000.0, dp1));
-		list.add(new Seller(4, "Donal Bluew", "donald@gmail.com", new Date(), 3580.0, dp2));
-
-		return list;
+		return dao.findAll();
 	}
 
 }
